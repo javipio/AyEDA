e647a6902ba8c256a0ca1b475e560ccf11903457
jpiop <jpiopadilla@gmail.com>
2022-05-09 14:38:43 +0100

    Añadida práctica 6

---
+++ p06/src/ABE.h
@@ -0,0 +1,37 @@
+/**
+ * @author Javier Padilla Pío
+ * @date 01/05/2022
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en ingeniería informática
+ * Curso: 2º
+ * Practice 6 - Implementación de árboles binarios equilibrados
+ * Email: alu0101410463@ull.edu.es
+ * ABE.h: Interfaz de la clase ABE concreción de la clase AB.
+ * Revision history:
+ *                01/05/2022 - Creation (first version) of the code
+ */
+
+#ifndef ABE_H_
+#define ABE_H_
+
+#include <iostream>
+#include <queue>
+
+#include "AB.h"
+
+template <class Key>
+class ABE : public AB<Key> {
+ public:
+  using AB<Key>::AB;
+
+  bool Insert(const Key k) override;
+  bool Search(const Key& k) const override;
+
+ private:
+  bool recursive_search(const Node<Key>* const start, const Key& k) const;
+};
+
+#include "ABE.tpp"
+
+#endif
---
+++ p06/src/main.cc
@@ -0,0 +1,60 @@
+/**
+ * @author Javier Padilla Pío
+ * @date 01/05/2022
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en ingeniería informática
+ * Curso: 2º
+ * Practice 6 - Implementación de árboles binarios equilibrados
+ * Email: alu0101410463@ull.edu.es
+ * main.cc: Punto de entrada para el programa.
+ * Revision history:
+ *                01/05/2022 - Creation (first version) of the code
+ */
+
+#include "ABE.h"
+
+int main(int argc, char* argv[]) {
+  AB<int>* tree = new ABE<int>;
+  int opt;
+  bool exit = false;
+
+  std::cout << "[0] Salir\n";
+  std::cout << "[1] Insertar clave\n";
+  std::cout << "[2] Buscar clave\n";
+  std::cout << "[3] Mostrar árbol inorden\n" << std::endl;
+
+  while (!exit) {
+    std::cout << "ABE $> ";
+    std::cin >> opt;
+
+    switch (opt) {
+      case 0:
+        std::cout << "\n\nExiting...\nGood bye!!" << std::endl;
+        exit = true;
+        break;
+      case 1:
+        int value;
+        std::cout << "Valor: ";
+        std::cin >> value;
+        std::cout << (tree->Insert(value)
+                          ? "Se añadió correctamente el elemento."
+                          : "Error añadiendo el elemento.")
+                  << std::endl;
+        std::cout << (*tree) << std::endl;
+        break;
+      case 2:
+        int key;
+        std::cout << "Clave: ";
+        std::cin >> key;
+        std::cout << (tree->Search(key)
+                          ? "Se encontró el elemento."
+                          : "El elemento no está contenido en el árbol")
+                  << std::endl;
+        break;
+      case 3:
+        tree->Inorder();
+        break;
+    }
+  }
+}
